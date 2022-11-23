1314372dccc6bc828008eec62780b467d192b913
priyanka-192 <118885557+priyanka-192@users.noreply.github.com>
2022-11-23 16:15:30 +0530

    breakfast menu
    breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,9 @@ import streamlit
 
 streamlit.title('Healthy Diner')
 streamlit.text('Welcome!!')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacado toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
